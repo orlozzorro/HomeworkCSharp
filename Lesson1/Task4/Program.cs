345f73c35ecd006f8f4d053247512db7df590505
Orlov_Denis <orlozzorro@yandex.ru>
2023-01-14 20:39:10 +0500

    add Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число больше 1 : ");
+string? stringValue1 = Console.ReadLine();
+
+int value1 = Convert.ToInt32(stringValue1);
+int i = 2;
+string output = "";
+if (value1 > 1)
+{
+    while(i <= value1)
+    {
+        output = output + $"{i} ";
+        i += 2;
+    } 
+    Console.WriteLine($"{value1} -> {output}");
+}
+else Console.WriteLine($"Введенное число {value1} меньше 1");
