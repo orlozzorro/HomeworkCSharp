a510dbf2bf0cb8ba23eed48527ef9cd3e3eb7887
Orlov_Denis <orlozzorro@yandex.ru>
2023-01-15 22:32:45 +0500

    add Homework2

--- Lesson2/Task2/Program.cs
+++ Lesson2/Task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int ThirdDigit(int number)
+{
+    int result = number;
+    while (result > 1000) // уменьшаем число до 3 знаков
+    {
+        result = result / 10;
+    }
+    result = result % 10;
+    return result;
+}
+
+Console.Write("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i>99) Console.WriteLine($"Во введеном числе {i} третья цифра: {ThirdDigit(i)}.");
+else Console.WriteLine($"Во введенном числе {i} третьей цифры нет");
--- Lesson2/Task3/Program.cs
+++ Lesson2/Task3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+String CheckVacation(int dayOfWeek)
+{
+    string result = "Рабочий. Нет";
+    if (dayOfWeek > 5) result = "Выходной. Да";
+    return result;
+}
+Console.Write("Введите цифру, обозначающую день недели: ");
+int i = Convert.ToInt32(Console.ReadLine());
+if (i > 0 && i < 8) Console.WriteLine(CheckVacation(i));
+else Console.WriteLine("Введена цифра, которая не может обозначать день недели");
