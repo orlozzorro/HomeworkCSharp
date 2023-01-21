dfe613116e9a3002209e1b778f6ee8635eb76496
orlozzorro <orlozzorro@yandex.ru>
2023-01-21 23:07:27 +0500

    add task3 lesson 3

--- Lesson3/Task3/Program.cs
+++ Lesson3/Task3/Program.cs
@@ -2,3 +2,24 @@
 
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+int Promt(string message)
+{
+    Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Coub(int a)
+{
+    return a * a * a;
+}
+
+int x = Promt("Введите число: ");
+int i = 1;
+while (i <= x)
+{
+    if (i == 1) Console.Write($"{x} -> ");
+    Console.Write(Coub(i) + " ");
+    i++;
+}
+Console.WriteLine();
