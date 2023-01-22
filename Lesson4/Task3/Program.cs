3361a1a37ad33df0291a643985c130dea9ff9248
Orlov_Denis <orlozzorro@yandex.ru>
2023-01-22 18:22:59 +0500

    add task3

--- Lesson4/Task3/Program.cs
+++ Lesson4/Task3/Program.cs
@@ -7,3 +7,30 @@ int Promt(string message)
     Console.Write($"{message} > ");
     return Convert.ToInt32(Console.ReadLine());
 }
+int[] doRndArray(int arraySize, int minValue, int maxValue)
+{
+    int[] arr = new int[arraySize];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+void OutPut(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+}
+
+int arraySize = Promt("Введите размерность массива");
+
+if (arraySize > 0) 
+{
+    int minValue = Promt("Введите пороговое случайное значение (включая пороговое значение)");
+    int maxValue = Promt("Введите пороговое случайное значение (включая пороговое значение)");
+    if(maxValue > minValue) OutPut(doRndArray(arraySize, minValue, maxValue));
+    else OutPut(doRndArray(arraySize, maxValue, minValue));
+}
+else System.Console.WriteLine("Ошибочная размерность массива");
