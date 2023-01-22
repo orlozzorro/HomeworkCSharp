06f657060dd990f797d4bcc5efed82b728b65b0e
Orlov_Denis <orlozzorro@yandex.ru>
2023-01-22 18:03:28 +0500

    add task2

--- Lesson4/Task2/Program.cs
+++ Lesson4/Task2/Program.cs
@@ -8,3 +8,17 @@ int Promt(string message)
     Console.Write($"{message} > ");
     return Convert.ToInt32(Console.ReadLine());
 }
+
+int MakeSum(int number)
+{
+    int result = 0;
+    while(number > 0)
+    {
+        result += number % 10; 
+        number /= 10;
+    }
+    return result;
+}
+
+int number = Promt("Введите число");
+System.Console.WriteLine($"Введено число {number}. сумма цифр в числе = {MakeSum(number)}");
