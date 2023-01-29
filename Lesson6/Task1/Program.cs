a24813fd22921dd3fccd36fd56a6caa8a05e013a
Denis O <orlozzorro@yandex.ru>
2023-01-29 20:08:24 +0500

    add Lesson 6

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// // Задача 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int Promt(string message)
+{
+    Console.Write($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] TakeNumbers(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Promt($"Введите число [{i + 1}] = ");
+    }
+    return array;
+}
+int CountPositive(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
+
+
+
+
+
+int count = Promt("Какое количество чисел вы будите вводить >");
+if (count > 0) System.Console.WriteLine($"Введено {CountPositive(TakeNumbers(count))} положительных число/числа/чисел");
+else System.Console.WriteLine("не может быть меньше 0");
---
+++ Lesson6/Task3/Program.cs
@@ -0,0 +1,67 @@
+﻿// Найдите максимальное значение в матрице по каждой строке, получите сумму этих максимумов. 
+// Затем найдите минимальное значение по каждой колонке,получите сумму этих минимумов. 
+// Затем из первой суммы (с максимумами) вычтите вторую сумму(с минимумами)
+// 1 2 3
+// 3 4 5
+// 3+5=8, 1+2+3=6, 8-6=2
+
+
+int[,] GenerateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = rnd.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+int SumMaxFromArray(int[,] array)
+{
+    int sumMax = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int maxValue = array[i,0];
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           if (array[i,j] > maxValue) maxValue = array[i,j];
+        }
+        sumMax += maxValue;
+    }
+    return sumMax;
+}
+
+int MinValuesInColumns(int[,] array)
+{
+    int sumMin = 0;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        int minValue = array[0, j];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+           if (array[i,j] < minValue) minValue = array[i,j];
+        }
+        sumMin += minValue;
+    }
+    return sumMin;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           System.Console.Write($"{array[i,j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array = GenerateArray(3, 5);
+PrintArray(array);
+System.Console.WriteLine($"сумма максимумов Строк = {SumMaxFromArray(array)}. Сумма минимумов столбцов {MinValuesInColumns(array)}, разность = {SumMaxFromArray(array) - MinValuesInColumns(array)}");
+
